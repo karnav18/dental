12b9788570ee3e3b8363c36b40d8f559abef8021
karnav18 <109799872+karnav18@users.noreply.github.com>
2022-08-04 20:13:03 +0530

    initial website

---
+++ JS/dist/main.dev.js
@@ -0,0 +1,35 @@
+"use strict";
+
+// actvi header
+var nav = document.querySelector(".navigation-wrap");
+
+window.onscroll = function () {
+  if (document.documentElement.scrollTop > 20) {
+    nav.classList.add("scroll-on");
+  } else {
+    nav.classList.remove("scroll-on");
+  }
+}; // nav hide
+
+
+var navBar = document.querySelectorAll('.nav-link');
+var navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  });
+}); // SMTP
+
+function SendEmail() {
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "karnavprajapati@gmail.com",
+    Password: "kartavya13",
+    To: 'karnavprajapati@gmail.com',
+    From: document.getElementById("email").value,
+    Subject: "Appointment",
+    Body: "Name:" + document.getElementById("name").value + "<br> Email: " + document.getElementById("email").value + "<br> Phone:" + document.getElementById("phone").value + "<br> Date-Time: " + document.getElementById("datetime").value + "<br> Massage:" + document.getElementById("Massage").value
+  }).then(function (message) {
+    return alert("Your appointment is success");
+  });
+}
